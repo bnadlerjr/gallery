80698c5585213a9ebc936afffaf359ea68106807
Bob Nadler <bnadlerjr@gmail.com>
2014-11-28 10:40:56 -0500

    Display gallery previews.

--- src/gallery/routes/home.clj
+++ src/gallery/routes/home.clj
@@ -1,10 +1,11 @@
 (ns gallery.routes.home
   (:require [compojure.core :refer :all]
             [gallery.views.layout :as layout]
-            [noir.session :as session]))
+            [noir.session :as session]
+            [gallery.routes.gallery :refer [show-galleries]]))
 
 (defn home []
-  (layout/common [:h1 "Hello " (session/get :user)]))
+  (layout/common (show-galleries)))
 
 (defroutes home-routes
   (GET "/" [] (home)))
